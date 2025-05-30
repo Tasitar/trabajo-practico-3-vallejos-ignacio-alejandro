661295cce6627109cc13129b8aae61ff221eec9a
alejandroIV <alejandroiv177771@gmail.com>
2025-05-30 02:57:26 -0300

    cree la funcion cargarDatos, esta sin testear

--- assets/js/scrip.js
+++ assets/js/scrip.js
@@ -1,1 +1,20 @@
-console.log ("")
+const btnBuscar = document.getElementById("btn-buscar");
+const urlDragonBall = "https://dragonball-api.com/api/characters";
+
+const cargarDatos = async (urlDragonBall) => {
+    try {
+        const response = await fetch(urlDragonBall);
+
+        if (!response.ok){
+            throw new Error("ERROR EN LA API");
+        }
+
+        const data = await response.json();
+
+        return data;
+    }
+    catch (error) {
+        console.log(error);
+    }
+};
+
